2a4196f5f28f98052d5a7800c62366634c5d855f
justbegginer <blackcloudsinthesky@protonmail.com>
2022-06-29 16:01:00 +0300

    fix ZeroBug, delete log output, replace if-else to switch-case

--- src/main/java/org/springMVC/controller/CalculatorController.java
+++ src/main/java/org/springMVC/controller/CalculatorController.java
@@ -3,10 +3,7 @@ package org.springMVC.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Objects;
 
 @Controller
 public class CalculatorController {
@@ -15,29 +12,28 @@ public class CalculatorController {
                              @RequestParam(name = "first") int firstOperator,
                              @RequestParam(name = "second") int secondOperator,
                              Model model){
-        int answer;
-        System.out.println("OPERATION "+operation);
-        if (Objects.equals(operation, "add")){
-            answer = firstOperator+secondOperator;
-        }
-        else if(Objects.equals(operation, "subtract")){
-            answer = firstOperator - secondOperator;
-        }
-        else if(Objects.equals(operation, "multiply")){
-            answer = firstOperator*secondOperator;
-        }
-        else if(Objects.equals(operation, "divide")){
-            answer = firstOperator/secondOperator;
-        }
-        else{
-            answer = 0;
-        }
-        if (answer == 0){
-            model.addAttribute("answer", "Error: incorrect operation");
-        }
-        else{
-            model.addAttribute("answer",
-                    "Answer is " + answer);
+        final String answer = "answer";
+        switch (operation){
+            case "add":
+                model.addAttribute(answer,
+                        "Answer is " +(firstOperator+secondOperator));
+                break;
+            case "multiply":
+                model.addAttribute(answer,
+                        "Answer is " + (firstOperator*secondOperator));
+                break;
+            case "divide":
+                model.addAttribute(answer,
+                        "Answer is " + (firstOperator/secondOperator));
+                break;
+            case "subtract":
+                model.addAttribute(answer,
+                        "Answer is " + (firstOperator - secondOperator));
+                break;
+            default:
+                model.addAttribute(answer,
+                        "Error: incorrect operation");
+                break;
         }
         return "calculator";
 
